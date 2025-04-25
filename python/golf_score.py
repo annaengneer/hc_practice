9ab0b9ade73853e7d20e4dffeaa7f9dbde0592b8
anna <kokoan438@gmail.com>
2025-04-25 19:23:53 +0900

    edit golf_score.py

--- python/golf_score.py
+++ python/golf_score.py
@@ -1,9 +1,8 @@
 import sys
-result_list = []
+result_dict = {4:'コンドル',-3:'アルバトロス',-2:'イーグル',-1:'バーティ',0:'パー'}
 sum_list = [-4, -3, -2, -1, 0, 1]
-t = []
-w = []
-r = []
+result_list = []
+
 # cat case_1.txt | python golf_score.py実行したら、イーグル,バーディ,コンドル,2ボギー,3ボギー,バーディ,ボギー,ホールインワン,ボギー,2ボギー,アルバトロス,ボギー,3ボギー,バーディ,ボギー,ボギー,バーディ,ボギーが返ってくる
 
 # cat case_1.txt(ファイルの内容を出力する) |
@@ -11,21 +10,16 @@ r = []
 # python golf_score.py(標準出力を受け取る)実行する
 
 # 標準出力を受け取る(4,4,5,3,5,4,4,3,4,4,5,4,4,3,4,4,3,5,2,3,1,5,8,3,5,1,5,6,2,5,7,2,5,5,2,6)
-for i in range(2):
-    input_line = str(input())
+input_line = sys.stdin.readlines()
     
-    # print(input_line)
 # 一行目と二行目を別々の変数に格納する(一行目：4,4,5,3,5,4,4,3,4,4,5,4,4,3,4,4,3,5, 2行目:2,3,1,5,8,3,5,1,5,6,2,5,7,2,5,5,2,6)
-    s = input_line.split(',')
-    for z in s:
-        t.append(z)
-        w.append(z)
-x = t[:18]
-y = w[18:]
-    
+line1 = input_line[0].strip()
+line2 = input_line[1].strip()
+
 # 一行目と二行目をint型のリストに変換する(x =[4,4,5,3,5,4,4,3,4,4,5,4,4,3,4,4,3,5],y = [2,3,1,5,8,3,5,1,5,6,2,5,7,2,5,5,2,6])
-x = [int(v) for v in x]
-y = [int(l) for l in y]
+x = [int(n) for n in line1.split(',')]
+y = [int(n) for n in line2.split(',')]
+
 
 # for文で繰り返し処理する
 # y[0]-x[0]を計算する(-2)
@@ -36,18 +30,8 @@ for d, f in zip(r,y):
         result_list.append('ホールインワン')
     elif d >= 2:
         result_list.append(f'{d}ボギー')
-    elif d == -4:
-        result_list.append('コンドル')
-    elif d == -3:
-        result_list.append('アルバトロス')
-    elif d == -2:
-        result_list.append('イーグル')
-    elif d ==-1:
-        result_list.append('バーディ')
-    elif d == 0:
-        result_list.append('パー')
     else:
-        result_list.append('ボギー')
+        result_list.append(result_dict.get(d, 'ボギー'))
 print(result_list)
    
 # イーグル,バーディ,コンドル,2ボギー,3ボギー,バーディ,ボギー,ホールインワン,ボギー,2ボギー,アルバトロス,ボギー,3ボギー,バーディ,ボギー,ボギー,バーディ,ボギーが返ってくる
