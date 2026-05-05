34fa51713dbc5f9b587f414fab397fbf98848b4b
anna <kokoan438@gmail.com>
2026-05-05 15:09:16 +0900

    refactor: DB接続情報を環境変数から取得するように修正

--- go_insert_db/main.go
+++ go_insert_db/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
@@ -21,8 +22,17 @@ type User struct {
 }
 
 func main() {
+	godotenv.Load()
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
 
-	connStr := "host=localhost port=5432 user=insert password=postgres dbname=insert sslmode=disable"
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
